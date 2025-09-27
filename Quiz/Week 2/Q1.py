60ddc1ca5344cf6e6919ece158a71317a3d8537d
John <3669213934@qq.com>
2025-09-28 01:44:05 +0800

    Add quiz of Week 2

---
+++ Quiz/Week 2/Q1.py
@@ -0,0 +1,14 @@
+# Write a program that outputs the following no matter what number you enter. 
+
+# Test 1
+
+# Number: 7
+#  Test
+
+# Test 2
+
+# Number: 13
+#  Test
+
+n = input("Number: ")
+print(" Test")
---
+++ Quiz/Week 2/Q2.py
@@ -0,0 +1,14 @@
+# Write a program that outputs Test n times, where n is the number that you enter. 
+
+# Test 1
+
+# Number: 7
+# TestTestTestTestTestTestTest
+
+# Test 2
+
+# Number: 13
+# TestTestTestTestTestTestTestTestTestTestTestTestTest
+
+n = int(input("Number: "))
+print ("Test" * n)
