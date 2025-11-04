b03c3176e014dacbb26bac34a04d886ee9471e77
John <u3628929@connect.hku.hk>
2025-11-04 16:28:00 +0800

    feat: Add illustrations for Quiz for Week 10

--- Quiz/src/Week 10/Q1.py
+++ Quiz/src/Week 10/Q1.py
@@ -1,3 +1,38 @@
+'''
+1. Binary Strings
+Problem Description
+
+Given an integer n, print out all possible cases of n-bit binary strings in increasing order.
+
+Hint: use recursion
+
+Sample Input & Output
+
+Case 1
+
+Input:
+
+2
+Output:
+
+[0, 0]
+[0, 1]
+[1, 0]
+[1, 1]
+Input:
+
+3
+Output:
+
+[0, 0, 0]
+[0, 0, 1]
+[0, 1, 0]
+[0, 1, 1]
+[1, 0, 0]
+[1, 0, 1]
+[1, 1, 0]
+[1, 1, 1]
+'''
 def binStr(n:int, prev:list):
     if len(prev) == n:
         print(prev)    
